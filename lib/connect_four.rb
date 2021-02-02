eaa52072eb7e1dafcadc5dbd5d38eea8850cc44f
Cheng Shao <cheng.shao38@gmail.com>
2021-02-01 22:45:46 -0500

    Initial commit

---
+++ lib/connect_four.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: false
+
+class ConnectFour
+
+  def intialize
+    @player_1 = Player.new
+    @player_2 = Player.new
+    @turn_player = @player_1
+    @gameboard = Gameboard.new
+  end
+
+  def play_game
+    
+  end
+
+  def win?
+  end
+
+  def full?
+  end
+
+  def load_game
+  end
+
+
+end
